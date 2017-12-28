b6be639f9c63718530f6069d12c43b995192803e
ldover <luka.dover@gmail.com>
2017-12-28 12:56:48 +0100

    Refactored BufferManagers. Now buffers are initialized by webgl-obj-loader's utils method

--- src/shader/buffer-manager.js
+++ src/shader/buffer-manager.js
@@ -11,71 +11,7 @@ export class BufferManager {
         this.mesh = mesh;
     }
 
-    initializeMeshBuffer() {
-        throw "This method should be implemented in a subclass";
-    }
-}
-
-export class VertexBufferManager extends BufferManager {
-    constructor(mesh){
-        super(mesh);
-    }
-
-    initializeMeshBuffer() {
-        let buffer = glContext.createBuffer();
-        glContext.bindBuffer(glContext.ARRAY_BUFFER, buffer);
-        glContext.bufferData(glContext.ARRAY_BUFFER, new Float32Array(this.mesh.vertices), glContext.STATIC_DRAW);
-        buffer.itemSize = 3;
-        buffer.numItems = this.mesh.vertices.length / 3;
-
-        return buffer;
-    }
-}
-
-export class TextureBufferManager extends BufferManager {
-    constructor(mesh){
-        super(mesh);
-    }
-
-    initializeMeshBuffer() {
-        let buffer = glContext.createBuffer();
-        glContext.bindBuffer(glContext.ARRAY_BUFFER, buffer);
-        glContext.bufferData(glContext.ARRAY_BUFFER, new Float32Array(this.mesh.textures), glContext.STATIC_DRAW);
-        buffer.itemSize = 2;
-        buffer.numItems = this.mesh.textures.length / 2;
-
-        return buffer;
-    }
-}
-
-export class NormalBufferManager extends BufferManager {
-    constructor(mesh){
-        super(mesh);
-    }
-
-    initializeMeshBuffer() {
-        let buffer = glContext.createBuffer();
-        glContext.bindBuffer(glContext.ARRAY_BUFFER, buffer);
-        glContext.bufferData(glContext.ARRAY_BUFFER, new Float32Array(this.mesh.vertexNormals), glContext.STATIC_DRAW);
-        buffer.itemSize = 3;
-        buffer.numItems = this.mesh.vertexNormals.length / 3;
-
-        return buffer;
-    }
-}
-
-export class IndexBufferManager extends BufferManager {
-    constructor(mesh){
-        super(mesh);
-    }
-
-    initializeMeshBuffer() {
-        let buffer = glContext.createBuffer();
-        glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, buffer);
-        glContext.bufferData(glContext.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.mesh.indices), glContext.STATIC_DRAW);
-        buffer.itemSize = 1;
-        buffer.numItems = this.mesh.indices.length;
-
-        return buffer;
+    initializeMeshBuffers() {
+        OBJ.initMeshBuffers(glContext, this.mesh);
     }
 }
